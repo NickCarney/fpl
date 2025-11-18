3be6fb6af0dd90bd3361e5a1d3c0bd14b5d77f04
Nick Carney <npcarney@uvm.edu>
2025-11-18 15:38:22 -0700

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { Analytics } from "@vercel/analytics/next";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
