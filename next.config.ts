26cce636e7de4c3e1965399cc14e155e64acde2f
nickcarney <npcarney@uvm.edu>
2025-11-21 23:47:26 -0700

    add @next/bundle-analyzer to next config and only load jsdom and mssql in server

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-export default nextConfig;
+const nextConfig: NextConfig = {
+  serverExternalPackages: ['jsdom', 'mssql'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'jsdom': false,
+        'mssql': false,
+      };
+    }
+    return config;
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
